df64f5a22b591482ac70dae2189997f31a566327
detmitchell <detmitchell@gmail.com>
2016-06-09 15:27:05 -0400

    Implemented search function

--- test/cloudant.spec.js
+++ test/cloudant.spec.js
@@ -4,9 +4,9 @@ var Cloudant = require('../lib/cloudant');
 
 var cloudant = new Cloudant({
 	cred: {
-		account: 'account',
-		username: 'username',
-		password: 'password'
+		account: 'acc',
+		username: 'user',
+		password: 'pass'
 	},
 	dbname: 'db'
 });
@@ -24,7 +24,6 @@ describe('cloudant', function () {
 		expect(cloudant.createDocument).to.be.a('function');
 		var params = {
 			objToInsert: {
-				_id: 'test_id',
 				a: 1,
 				b: 'soothsayer'
 			}
@@ -45,7 +44,7 @@ describe('cloudant', function () {
 	it('.readDocument', function (done) {
 		expect(cloudant.readDocument).to.be.a('function');
 		var params = {
-			_id: 'test_id'
+			_id: '86f72603fb4a778bb84c1251f04f598f'
 		}
 		cloudant.readDocument(params, function (err, response) {
 			if (err) console.log(err);
@@ -62,14 +61,14 @@ describe('cloudant', function () {
 		var dat;
 		expect(cloudant.updateDocument).to.be.a('function');
 
-		cloudant.readDocument({ _id: 'test_id' }, function (err, response) {
+		cloudant.readDocument({ _id: '86f72603fb4a778bb84c1251f04f598f' }, function (err, response) {
 			if (err) console.log(err);
 			else {
 				dat = response;
 
 				var params = {
 					objToUpdate: {
-						_id: 'test_id',
+						_id: '86f72603fb4a778bb84c1251f04f598f',
 						value: "new val",
 						_rev: dat._rev
 					}
@@ -92,30 +91,60 @@ describe('cloudant', function () {
 		});
 	})
 
-	it('.deleteDocument', function (done) {
+	// it('.deleteDocument', function (done) {
+	// 	this.timeout(5000);
+	// 	var dat;
+	// 	expect(cloudant.deleteDocument).to.be.a('function');
+	// 	cloudant.readDocument({ _id: 'test_id' }, function (err, response) {
+	// 		if (err) console.log(err);
+	// 		else {
+	// 			dat = response;
+
+	// 			var params = {
+	// 				objToDelete: {
+	// 					_id: 'test_id',
+	// 					_rev: dat._rev
+	// 				}
+	// 			}
+	// 			cloudant.deleteDocument(params, function (err, response) {
+	// 				if (err) console.log('Delete failed, ensure that you have admin access');
+	// 				else {
+	// 					var data = response;
+	// 					console.log(data);
+	// 				}
+	// 				done();
+	// 			})
+	// 		}
+	// 	});
+	// })
+
+	it('.search', function (done) {
 		this.timeout(5000);
-		var dat;
-		expect(cloudant.deleteDocument).to.be.a('function');
-		cloudant.readDocument({ _id: 'test_id' }, function (err, response) {
+		expect(cloudant.search).to.be.a('function');
+		var params = {
+			field: 'a',
+			value: 1
+		}
+		cloudant.search(params, function (err, response) {
 			if (err) console.log(err);
 			else {
-				dat = response;
-
-				var params = {
-					objToDelete: {
-						_id: 'test_id',
-						_rev: dat._rev
-					}
-				}
-				cloudant.deleteDocument(params, function (err, response) {
-					if (err) console.log('Delete failed, ensure that you have admin access');
-					else {
-						var data = response;
-						console.log(data);
-					}
-					done();
-				})
+				console.log(response.docs.length);
+				expect(response).to.be.a('object');
+				done();
 			}
+		})
+	})
+
+	it('.createIndex', function(done){
+		this.timeout(5000);
+		expect(cloudant.createIndex).to.be.a('function');
+		cloudant.createIndex('index_a','value.a',function(err,response){
+			if(err) console.log(err);
+			else{
+				console.log('Index created: %s',response.result);
+			}
+			done();
 		});
+
 	})
 })
